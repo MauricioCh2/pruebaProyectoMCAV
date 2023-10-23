9697d07199dc97df801c9aa8ca650c7c5d5c06d2
MAURICIO CHAVES CHAVES <mauricio.chaves.chaves@est.una.ac.cr>
2023-10-23 00:41:25 -0600

    item pt1

--- Model/Item.h
+++ Model/Item.h
@@ -5,9 +5,30 @@
 #ifndef PRUEBAPROYECTOMCAV_ITEM_H
 #define PRUEBAPROYECTOMCAV_ITEM_H
 
-
+#include "Utilities.h"
 class Item {
+private:
+    string ID;
+    string Nombre;
+    double precio;
+    float traccion;
+    float velocidad;
+    float potencia;
+public:
+    Item();
 
+    virtual const string &getId() const  = 0;
+    virtual void setId(const string &id) = 0;
+    virtual const string &getNombre() const  = 0;
+    virtual void setNombre(const string &nombre) = 0;
+    virtual double getPrecio() const = 0;
+    virtual void setPrecio(double precio)  = 0;
+    virtual float getTraccion() const  = 0;
+    virtual void setTraccion(float traccion)  = 0;
+    virtual float getVelocidad() const  = 0;
+    virtual void setVelocidad(float velocidad)  = 0;
+    virtual float getPotencia() const = 0;
+    virtual void setPotencia(float potencia) = 0;
 };
 
 
